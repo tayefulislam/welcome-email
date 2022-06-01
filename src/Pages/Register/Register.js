f1b62dd883beffe16366f38c1b8fcd5aa47cac8a
tayeful912 <kazitayefulislam12@gmail.com>
2022-06-01 17:21:25 +0600

    register page added

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,13 +1,79 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
 
 
     return (
-        <div>
+        <div className='flex justify-center items-center mt-12'>
+            <div className="card w-96 bg-gray-200 shadow-xl">
+                <div className="card-body ">
+                    <h2 className="text-center text-2xl font-bold">Sign Up</h2>
+
+
+                    <form >
+
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+
+                            </label>
+
+                            <input type="text"
+                                placeholder="Your Name"
+                                className="input input-bordered w-full max-w-xs"
+
+
+                            />
 
 
 
+                        </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+
+                            </label>
+
+                            <input type="text"
+                                placeholder="Email Address"
+                                className="input input-bordered w-full max-w-xs"
+
+
+
+                            />
+
+
+
+                        </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+
+                            </label>
+
+                            <input type="password"
+                                placeholder="Password"
+                                className="input input-bordered w-full max-w-xs"
+
+
+                            />
+                        </div>
+
+
+
+
+
+
+
+
+                        <input className='btn  w-full max-w-xs' type="submit" value="Signup" />
+                    </form>
+
+
+
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/Pages/User/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-center text-2xl'> Welcome to Home Page</h1>
+
+        </div>
+    );
+};
+
+export default Home;
